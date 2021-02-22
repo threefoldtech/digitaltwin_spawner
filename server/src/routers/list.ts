11143b23472cf7c89b8ca08f404426dbd4347ee4
tobiaschielens <tobias.chielens@gmail.org>
2021-02-22 23:45:11 +0100

    start of yggdrasil

--- server/src/routers/list.ts
+++ server/src/routers/list.ts
@@ -1,9 +1,24 @@
-import {Router} from "express";
-import {fetchChatList} from "../service/dockerService";
+import { Router } from "express";
+import axios from "axios";
+import { fetchChatList } from "../service/dockerService";
 
 export const router = Router();
 
-router.get('/', async (request, respose) => {
-    const digitaltwins = await fetchChatList()
-    respose.json(digitaltwins)
+router.get("/", async (request, respose) => {
+  const digitaltwinNames = await fetchChatList();
+  const digitalTwinProfiles = digitaltwinNames.map(async (digitaltwin) => {
+    const yggdrasilAddress = await axios.get(
+      `http://${digitaltwin}-chat/api/yggdrasil_address`
+    );
+    console.log(digitaltwin);
+    console.log(yggdrasilAddress.data);
+    return {
+      id: digitaltwin,
+      location: yggdrasilAddress.data,
+    };
+  });
+  Promise.all(digitalTwinProfiles).then((data) => {
+    console.log("data", data);
+    respose.json(data);
+  });
 });
